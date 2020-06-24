98677a9ebda25ac9d0da1edb6de3c9b967936462
Daniil Sheftelevich <shftlvch@gmail.com>
2020-06-24 12:01:12 +0300

    pause and bg

---
+++ precache-manifest.6b452e8747b8d0235f8c44544770264d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b36d094c3783e88ab025b3e4fba5d44d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc30c0b28229864e7898",
+    "url": "/static/css/main.8b2fbdcc.chunk.css"
+  },
+  {
+    "revision": "f0e0bee2284dec0298bd",
+    "url": "/static/js/2.24fd9281.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.24fd9281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc30c0b28229864e7898",
+    "url": "/static/js/main.02d5aaa5.chunk.js"
+  },
+  {
+    "revision": "17cd325e1b92610b3e55",
+    "url": "/static/js/runtime-main.aeeb3b39.js"
+  },
+  {
+    "revision": "6d37d5b41056f78d6b30eeae3b61baf9",
+    "url": "/static/media/source.6d37d5b4.mp3"
+  }
+]);
